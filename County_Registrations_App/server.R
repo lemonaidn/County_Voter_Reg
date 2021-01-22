b7d434aa2e8ef680bd16ebce73090a1b9aae8deb
Aidan Hughes <72153806+lemonaidn@users.noreply.github.com>
2021-01-22 15:06:19 -0500

    Removed plots

--- County_Registrations_App/server.R
+++ County_Registrations_App/server.R
@@ -20,25 +20,6 @@ df_2020 = read.csv('df_2020.csv')
 
 
 function(input, output) {
-    output$onesix=renderPlot(
-        df_2016 %>%
-            filter(County==input$County) %>%
-            ggplot(aes(x = Reporting_Date,
-                       y = Percent_Monthly_Change,
-                       fill = Percent_Monthly_Change > 0)) +
-            geom_col() + theme_economist_white() + scale_fill_tableau() + guides(fill = "none")
-        
-        
-    )
-    
-    output$twozero=renderPlot(
-        df_2020 %>%
-            filter(County==input$County) %>%
-            ggplot(aes(x = Reporting_Date,
-                       y = Percent_Monthly_Change,
-                       fill = Percent_Monthly_Change > 0)) +
-            geom_col() + theme_economist_white() + scale_fill_tableau() + guides(fill = "none")
-    )
     
     output$tableonesix=renderTable(
         df_2016 %>%
--- County_Registrations_App/ui.R
+++ County_Registrations_App/ui.R
@@ -25,49 +25,29 @@ fluidPage(theme = shinytheme('cerulean'),
           titlePanel("NJ Voter Registration"),
           
           sidebarLayout(
-              # Show a plot of the generated distribution
+              # Drop-down menu with region options
               
               mainPanel(
-                  selectizeInput(inputId = 'County', label= 'Statewide',
+                  selectizeInput(inputId = 'County', label= 'Region',
                                  choices = df_2016$County),
+            
               ),
+              
               mainPanel(
                   tabsetPanel(
                       
                       tabPanel('Home',
-                               'This web app provides insights and demonstrates the impact caused by COVID on voter registration rates in NJ.',
                                "Data is sourced from the NJ Division of Elections Voter Registration Statistics Archive, which can be accessed at https://www.state.nj.us/state/elections/election-information-svrs.shtml",
-                               "An analysis of the data reveals the intense impact that COVID had on the ability of NJ citizens to register to vote.",
-                               "While there was only one instance of near- or below-zero Percent Change in monthly registrations across all congressional districts in 2016, there were at least 12 such incidents in 2020.",
-                               "This was followed by an unprecedented rush of new registrations leading up to the 2020 voter registration deadline.",
-                               "These numbers suggest the importance of alternative registration methods for voters, and highlights the difficulty that citizens may have when in-person egistration is unavailable or inconvenient.",
                                "Please use the drop-down menu above to select whether to view statewide statistics, or data for a specific county.",
-                               "You can also select year, and whether to generate visualizations or data tables, using the blue tabs above.",
-                               "Requests and suggestions for additional functionality can be sent to hughes@nextsteppolitics.com"
                       ),
                       
-                      tabPanel('2016 Plot',
-                               'The following graph displays month-to-month % change in voter registration for your selected year and region.',
-                               'For example, % change on May 31, 2016 represents % change since the PREVIOUS reporting date, NOT cumulative growth since the beginning of the year.
-                (If dates on the x-axis appear compressed, please ensure the application window is fully maximized.)',
-                               plotOutput('onesix')
-                      ),
                       tabPanel('2016 Data',
-                               'The dataframe for your selection is provided here. Please note that although the dataframe includes voter reg statistics by
-                  party affiliation, the "Net Monthly Change" and "Percent Monthly Change" columns (and the graphic displayed in the "Plot" tab) are calculated from the "Total" column
-                  for all registrations, regardless of party affiliation. The option to calculate & visualize change by party affiliation may be added in a future update.',
+                               'The dataframe for your selection is provided here.',
                                tableOutput('tableonesix')
                       ),
-                      tabPanel('2020 Plot',
-                               'The following graph displays month-to-month % change in voter registration for your selected year and region.',
-                               'For example, % change on May 31, 2016 represents % change since the PREVIOUS reporting date, NOT cumulative growth since the beginning of the year.
-                (If dates on the x-axis appear compressed, please ensure the application window is fully maximized.)',
-                               plotOutput('twozero')
-                      ),
+
                       tabPanel('2020 Data',
-                               'The dataframe for your selection is provided here. Please note that although the dataframe includes voter reg statistics by
-                  party affiliation, the "Net Monthly Change" and "Percent Monthly Change" columns (and the graphic displayed in the "Plot" tab) are calculated from the "Total" column
-                  for all registrations, regardless of party affiliation. The option to calculate & visualize change by party affiliation may be added in a future update.',
+                               'The dataframe for your selection is provided here.',
                                tableOutput('tabletwozero')
                       )
                       
